31d700bc94c68bb4bab14e52eac4553fd3401cdf
Aleksander Zamojski <zamojski96@gmail.com>
2018-05-05 14:19:46 +0200

    Add PacketType class

---
+++ server/src/packet/PacketType.cpp
@@ -0,0 +1,34 @@
+#include "PacketType.h"
+
+namespace packet {
+
+    uint8_t packet::PacketType::toUint8(header header) {
+        return static_cast<uint8_t>(header);
+    }
+
+    packet::PacketType::header packet::PacketType::toPackeType(const uint8_t header) {
+        static std::unordered_map<uint8_t, PacketType::header> packetTypeMap = {
+                {static_cast<uint8_t>(PacketType::header::OPEN),            PacketType::header::OPEN},
+                {static_cast<uint8_t>(PacketType::header::CLOSE),           PacketType::header::CLOSE},
+                {static_cast<uint8_t>(PacketType::header::ACK_OK),          PacketType::header::ACK_OK},
+                {static_cast<uint8_t>(PacketType::header::ACK_ERR),         PacketType::header::ACK_ERR},
+                {static_cast<uint8_t>(PacketType::header::ALIVE),           PacketType::header::ALIVE},
+
+                {static_cast<uint8_t>(PacketType::header::OPEN_ENCR),       PacketType::header::OPEN_ENCR},
+                {static_cast<uint8_t>(PacketType::header::PUBLIC_KEY),      PacketType::header::PUBLIC_KEY},
+                {static_cast<uint8_t>(PacketType::header::SYMMETRIC_KEY),   PacketType::header::SYMMETRIC_KEY},
+                {static_cast<uint8_t>(PacketType::header::TEST_KEY),        PacketType::header::TEST_KEY},
+
+                {static_cast<uint8_t>(PacketType::header::OPEN_PROT),       PacketType::header::OPEN_PROT},
+                {static_cast<uint8_t>(PacketType::header::SET_NAME),        PacketType::header::SET_NAME},
+                {static_cast<uint8_t>(PacketType::header::ADD_FOLLOWER),    PacketType::header::ADD_FOLLOWER},
+                {static_cast<uint8_t>(PacketType::header::NEW_FOLLOWED),    PacketType::header::NEW_FOLLOWED},
+                {static_cast<uint8_t>(PacketType::header::REMOVE_FOLLOWER), PacketType::header::REMOVE_FOLLOWER},
+                {static_cast<uint8_t>(PacketType::header::REMOVE_FOLLOWED), PacketType::header::REMOVE_FOLLOWED},
+                {static_cast<uint8_t>(PacketType::header::MY_LOCATION),     PacketType::header::MY_LOCATION},
+                {static_cast<uint8_t>(PacketType::header::LOCATION),        PacketType::header::LOCATION},
+        };
+
+        return packetTypeMap.count(header) ? packetTypeMap.at(header) : PacketType::header::UNDEFINED;
+    }
+}
---
+++ server/src/packet/PacketType.h
@@ -0,0 +1,40 @@
+#ifndef LOCUS_PACKETTYPE_H
+#define LOCUS_PACKETTYPE_H
+
+#include <cstdint>
+#include <unordered_map>
+
+namespace packet {
+    struct PacketType {
+        enum header : uint8_t {
+            OPEN = 0,
+            CLOSE = 1,
+            ACK_OK = 2,
+            ACK_ERR = 3,
+            ALIVE = 4,
+
+            OPEN_ENCR = 8,
+            PUBLIC_KEY = 9,
+            SYMMETRIC_KEY = 10,
+            TEST_KEY = 11,
+
+            OPEN_PROT = 16,
+            SET_NAME = 17,
+            ADD_FOLLOWER = 18,
+            NEW_FOLLOWED = 19,
+            REMOVE_FOLLOWER = 20,
+            REMOVE_FOLLOWED = 21,
+            MY_LOCATION = 22,
+            LOCATION = 23,
+
+            UNDEFINED = 100
+        };
+
+        static uint8_t toUint8(PacketType::header header);
+
+        static PacketType::header toPackeType(uint8_t header);
+    };
+
+}
+
+#endif //LOCUS_PACKETTYPE_H
