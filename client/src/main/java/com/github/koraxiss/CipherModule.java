b76ccf2bf412112515e0faeb6feb653a42e35ffd
piropaolo <piropaolo27@gmail.com>
2018-05-15 15:04:50 +0200

    Add genaration, saving and loading of private and public keys

--- client/src/main/java/com/github/koraxiss/CipherModule.java
+++ client/src/main/java/com/github/koraxiss/CipherModule.java
@@ -4,42 +4,139 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.io.*;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class CipherModule {
     private static KeyPairGenerator keyPairGenerator;
     private static KeyPair keyPair;
-    private static Cipher cipherEncrypt;
-    private static Cipher cipherDecrypt;
+    private static Key serverPublicKey;
+    private static Key sessionKey;
 
-    static {
-        try {
-            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            cipherEncrypt = Cipher.getInstance("RSA");
-            cipherDecrypt = Cipher.getInstance("RSA");
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            e.printStackTrace();
-        }
+    private static Cipher encryptClientPrivate;
+    private static Cipher decryptClientPrivate;
 
+    private static Cipher encryptServerPublic;
+    private static Cipher decryptServerPublic;
+
+    private static Cipher encryptSession;
+    private static Cipher decryptSession;
+
+    private static State state;
+
+    enum State{
+        ESTABLISHING_CONNECTION,
+        CIPHERING_DATA
+    }
+
+    static KeyPair generateKeyPair() throws NoSuchAlgorithmException, IOException {
+        keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(512);
         keyPair = keyPairGenerator.generateKeyPair();
 
-        try {
-            cipherEncrypt.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
-            cipherDecrypt.init(Cipher.DECRYPT_MODE, keyPair.getPublic());
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        }
+        byte[] publicKey = keyPair.getPublic().getEncoded();
+        byte[] privateKey = keyPair.getPrivate().getEncoded();
+
+        File dir = new File("key");
+        if(!dir.exists())   dir.mkdir();
+
+        File publicKeyFile = new File("key/public_key");
+        File privateKeyFile = new File("key/private_key");
+
+        if(!publicKeyFile.exists())     publicKeyFile.createNewFile();
+        if(!privateKeyFile.exists())    privateKeyFile.createNewFile();
+
+        FileOutputStream publicKeyFOS = new FileOutputStream(publicKeyFile);
+        FileOutputStream privateKeyFOS = new FileOutputStream(privateKeyFile);
+
+        publicKeyFOS.write(publicKey);
+        publicKeyFOS.close();
+
+        privateKeyFOS.write(privateKey);
+        privateKeyFOS.close();
+
+        return keyPair;
     }
 
+    static KeyPair loadKeyPair() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        FileInputStream publicKeyFIS = new FileInputStream("key/public_key");
+        FileInputStream privateKeyFIS = new FileInputStream("key/private_key");
+
+        byte[] publicKeyBytes = new byte[publicKeyFIS.available()];
+        byte[] privateKeyBytes = new byte[privateKeyFIS.available()];
+
+        publicKeyFIS.read(publicKeyBytes);
+        publicKeyFIS.close();
+
+        privateKeyFIS.read(privateKeyBytes);
+        privateKeyFIS.close();
+
+        X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory pubKeyFactory = KeyFactory.getInstance("RSA");
+        PublicKey pubKey = pubKeyFactory.generatePublic(pubKeySpec);
+
+        PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        KeyFactory privKeyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privKey = privKeyFactory.generatePrivate(privKeySpec);
+
+        keyPair = new KeyPair(pubKey, privKey);
+        return keyPair;
+    }
+
+    public static void printKeyPair(){
+        System.out.println(keyPair.getPublic().toString());
+        System.out.println(keyPair.getPrivate().toString());
+    }
+
+    public static void loadServerPublic(){
+
+    }
+
+    public static void loadSessionKey(){
+
+    }
+
+    public static void initializeClientCiphers() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        encryptClientPrivate = Cipher.getInstance("RSA");
+        decryptClientPrivate = Cipher.getInstance("RSA");
+
+        encryptClientPrivate.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
+        decryptClientPrivate.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+    }
+
+    public static void initializeServerCiphers() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        encryptServerPublic = Cipher.getInstance("RSA");
+        decryptServerPublic = Cipher.getInstance("RSA");
+
+        encryptServerPublic.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+        decryptServerPublic.init(Cipher.DECRYPT_MODE, serverPublicKey);
+    }
+
+    public static void initializeSessionCiphers() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        encryptSession = Cipher.getInstance("AES");
+        decryptSession = Cipher.getInstance("AES");
+
+        encryptSession.init(Cipher.ENCRYPT_MODE, sessionKey);
+        encryptSession.init(Cipher.ENCRYPT_MODE, sessionKey);
+    }
+
+
     public static byte[] encrypt(byte[] buffer, int offset, int n) throws BadPaddingException, IllegalBlockSizeException {
-        return cipherEncrypt.doFinal(buffer, offset, n);
+        return encryptClientPrivate.doFinal(buffer, offset, n);
     }
 
     public static byte[] decrypt(byte[] buffer, int offset, int n) throws BadPaddingException, IllegalBlockSizeException {
-        return cipherDecrypt.doFinal(buffer, offset, n);
+        return decryptServerPublic.doFinal(buffer, offset, n);
+    }
+
+    public static State getState() {
+        return state;
+    }
+
+    public static void setState(State state) {
+        CipherModule.state = state;
     }
 }
--- client/src/test/java/com/github/koraxiss/CipherModuleTest.java
+++ client/src/test/java/com/github/koraxiss/CipherModuleTest.java
@@ -2,9 +2,14 @@ package com.github.koraxiss;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class CipherModuleTest {
     @Test
@@ -14,5 +19,22 @@ public class CipherModuleTest {
         byte[] decryptedText = CipherModule.decrypt(cipherText, 0, cipherText.length);
 
         assertEquals(plainText, Converter.byteToString(decryptedText));
+    }
+
+    @Test
+    public void generateKeyPairTest() throws IOException, NoSuchAlgorithmException {
+        CipherModule.generateKeyPair();
+    }
+
+    @Test
+    public void loadKeyPairTest() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        CipherModule.loadKeyPair();
+    }
+
+    @Test
+    public void keysUponGenerationAndLoadingAreTheSame() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyPair keyPair = CipherModule.generateKeyPair();
+        assertThat(CipherModule.loadKeyPair().getPublic().toString()).isEqualTo(keyPair.getPublic().toString());
+        assertThat(CipherModule.loadKeyPair().getPrivate().toString()).isEqualTo(keyPair.getPrivate().toString());
     }
 }
