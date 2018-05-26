cb4487eea6abcba6f2cc4695912dd8372c6ded48
piropaolo <piropaolo27@gmail.com>
2018-05-26 14:41:06 +0200

    Implement basic list activity

---
+++ android/app/src/main/java/com/volterra/tin/petarda/UserAdapter.java
@@ -0,0 +1,52 @@
+package com.volterra.tin.petarda;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private List<String> mData;
+    private LayoutInflater mInflater;
+
+    // Provide a suitable constructor
+    UserAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycler_view_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+    }
+
+    // Return the size of data set (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // Provide a reference to the views for each data item
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.user_name);
+        }
+    }
+}
